32e280b76b992f89498ee751b5aad27b699e5a8a
grjr02 <grjr02@outlook.com>
2020-06-03 14:08:17 -0400

    Project Start

---
+++ src/components/Folder.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Folder extends Component {
+    render() {
+        return (
+            <div>
+                Folder
+            </div>
+        )
+    }
+}

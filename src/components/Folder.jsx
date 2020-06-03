1089ac29f9639f9e6ce42203a1da5a9f6ff5fdfc
grjr02 <grjr02@outlook.com>
2020-06-03 15:09:22 -0400

    Toggle Functionality

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
 
 export default class Folder extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+            toggle: true,
+            toggleStatus : "Hide"
+
+        };
+
+    }
+    // componentDidMount(){
+    //     this.toggle();
+    // }
+
+    toggle = () => {
+        if(this.state.toggle){
+            this.setState({toggle:false, toggleStatus:"Show"})
+        }else{
+            this.setState({toggle:true, toggleStatus:"Hide"})
+        }
+    }
+
+    hideContents = () => {
+
+        
+
+    }   
     render() {
+
+        const {toggleStatus, toggle} = this.state;
+
         return (
             <div>
-                Folder
+
+                Home
+                { toggle &&
+                <ul>
+                    <li>File1</li>
+                    <li>File2</li>
+                    <li>File3</li>
+                </ul>
+                }
+                <br/>
+                <button onClick={()=>this.toggle()}>{toggleStatus}</button>
+
             </div>
         )
     }
